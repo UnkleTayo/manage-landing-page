fed5c3187ec95c55dcb15010d66d7cd40b04202b
Adetayo <akinsanyaadetayo@gmail.com>
2020-02-26 00:22:03 +0100

    mobile view almost done

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const hamburgerMenu = doc.querySelector('.hamburger');
 const mainNavMenu = doc.querySelector('.main-nav');
 const sectionOne = doc.querySelector('sec-1');
 const date = doc.querySelector('.date');
+const elem = document.querySelector('.main-carousel');
 
 hamburgerMenu.addEventListener('click', () => {
   mainNavMenu.classList.toggle('main-nav--active');
@@ -14,3 +15,15 @@ const today = new Date();
 const year = today.getFullYear();
 
 date.innerHTML = year;
+
+// var flkty = new Flickity(elem, {
+//   // options
+//   cellAlign: 'left',
+//   contain: true
+// });
+
+// // element argument can be a selector string
+// //   for an individual element
+// var flkty = new Flickity('.main-carousel', {
+//   // options
+// });
